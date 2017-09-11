9ffe56af7323a367336d958d446d1002cd0f037d
zwan <zhentian.wan@ssh.com>
2017-09-11 15:59:36 +0300

    init push

---
+++ src/components/Hello.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+    <button @click="clicked">Click</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({})
+export default class Hello extends Vue {
+  message: string = 'Welcome to Your Vue.js App'
+
+  get fullMessage() {
+    return `${this.message} from Typescript`
+  }
+
+  created() {
+    console.log('created');
+  }
+
+  clicked(){
+      console.log('clicked');
+    }
+}
+
+/*
+export default {
+  data () {
+    return {
+      message: 'Welcome to Your Vue.js App'
+    }
+  },
+
+  computed: {
+    fullMessage(){
+      return `${this.message} from Typescript`;
+    }
+  },
+
+  created() {
+    console.log('created');
+  },
+
+  methods: {
+    clicked(){
+      console.log('clicked');
+    }
+  }
+}*/
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
